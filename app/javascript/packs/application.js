03390a61a2e40e2d814ec62dbbd06ad0ece3fae4
Shaqal7 <shaqal7@gmail.com>
2019-09-26 15:02:26 +0200

    add nav menu

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,10 +4,10 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
+//= require jquery
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-// require jquery
 //= require semantic-ui
 
 
