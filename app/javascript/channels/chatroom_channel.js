82e7d1bf0800ea04e19a5529aa71b260207f6492
Shaqal7 <shaqal7@gmail.com>
2019-09-29 22:33:54 +0200

    some fix in chatroom_channel view

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -13,7 +13,7 @@ consumer.subscriptions.create("ChatroomChannel", {
   received(data) {
     // Called when there's incoming data on the websocket for this channel
     //$('#msg').append(data.content[0]);
-    $('#msg').append('<div class="event"><div class="content"><div class="summary"><em> ' + data.content[0] + '</em>:' + data.content[1].body + '<div class="time_to_right">' + data.content[2] + '</div></div></div></div>')
+    $('#msg').append('<div class="event"><div class="content"><div class="summary"><em>' + data.content[0] + '</em>: ' + data.content[1].body + '<div class="time_to_right">' + data.content[2] + '</div></div></div></div>')
     //$('#msg').append('<div class="message"> ' + data.content + '</div>')
   }
 });
