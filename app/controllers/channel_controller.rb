b64f14c7b3a75b3646d586c433379c329e5fa002
Shaqal7 <shaqal7@gmail.com>
2019-09-30 09:35:28 +0200

    add custon scope

--- app/controllers/channel_controller.rb
+++ app/controllers/channel_controller.rb
@@ -3,6 +3,6 @@ class ChannelController < ApplicationController
 
   def index
     @message = Message.new
-    @messages = Message.all
+    @messages = Message.custom_display
   end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,7 +4,6 @@ class MessagesController < ApplicationController
   def create
     message = current_user.messages.build(message_params)
     if message.save
-      #ActionCable.server.broadcast "chatroom_channel", content: [current_user.email, message, (message.created_at).strftime("%F %T" )]
       ActionCable.server.broadcast "chatroom_channel", content: message_render(message)
     else
       redirect_to root_path
